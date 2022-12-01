a051da1e56e6501e00a2438df2c302d28cb1f448
shrvya <shrivyashetty@gmail.com>
2022-12-01 11:16:09 +0530

    edit data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app=express();
 const path=require('path');
 const database=require('./database');
 const Product=require('./models/product')
+const methodOverride=require('method-override')
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 //  app.set('views',path.join(__filename,'views'));
 app.set('view engine','ejs')
@@ -16,15 +18,32 @@ app.get('/products/newprod',(req,res)=>{
     res.render('new.ejs')
     
 })
-app.post('/products',(req,res)=>{
-console.log(req.body);
-
+app.post('/products',async(req,res)=>{
+const newProduct= new Product(req.body) ;
+await newProduct.save();
+res.redirect(`/products/${newProduct._id}`)
 })
 app.get('/products/:id',async(req,res)=>{
     const {id}=req.params
     const result=await Product.findById(id)
   
      res.render('details.ejs',{result});
+ })
+ app.get('/products/:id/edit',async(req,res)=>{
+    const {id}=req.params
+    const result=await Product.findById(id)
+     res.render('edit.ejs',{result});
+ })
+ app.put('/products/:id',async(req,res)=>{
+    const {id}=req.params
+    const result=await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+    res.redirect(`/products/${result._id}`)
+ })
+ app.delete('/products/:id',async(req,res)=>{
+    const {id}=req.params
+    console.log(req.params)
+     const result=await Product.findByIdAndDelete(id)
+     res.redirect('/products')
  })
 
  
